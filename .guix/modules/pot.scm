b9cf5b1896f11e11ccd380440e8e2b8cb8e1e397
Giacomo Leidi <giacomo.leidi@suse.com>
2023-11-08 02:19:10 +0100

    Add guix.scm.

---
+++ .guix/modules/pot.scm
@@ -0,0 +1,63 @@
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+
+(define-module (pot)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (guix build-system pyproject)
+  #:use-module (guix gexp)
+  #:use-module (guix git-download)
+  #:use-module ((guix licenses)
+                #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix utils)
+  ;; Temporary, when python-textual-0.41 is upstreamed
+  ;; this dependency can be dropped.
+  #:use-module (small-guix packages python-xyz)
+  #:use-module (ice-9 rdelim)
+  #:use-module (ice-9 popen)
+  #:use-module (srfi srfi-1))
+
+(define %source-dir (string-append (current-source-directory)
+                                   "/../.."))
+
+(define %source-commit
+  (let ((commit (read-line
+                  (open-input-pipe "git show HEAD | head -1 | cut -d ' ' -f 2"))))
+    (if (and (list? commit)
+             (eq? (first commit) #\eot))
+        ;; We are in guix pull.
+        "0000000000000000000000000000000000000000"
+        commit)))
+
+(define-public pot.git
+  (let ((version (with-input-from-file
+                     (string-append %source-dir
+                                    "/pot/res/VERSION")
+                   read-line))
+        (revision "0"))
+    (package
+     (name "pot.git")
+     (version (git-version version revision %source-commit))
+     (source
+      (local-file %source-dir "pot-source"
+                  #:recursive? #t
+                  #:select? (git-predicate %source-dir)))
+     (build-system pyproject-build-system)
+     (arguments
+      (list #:phases #~(modify-phases %standard-phases
+                                      (add-after 'unpack 'patch-version
+                                                 (lambda _
+                                                   (with-output-to-file "pot/res/VERSION"
+                                                     (lambda _
+                                                       (display #$version)))))
+                                      ;; There are no unit tests currently.
+                                      (delete 'check))))
+     (native-inputs (list poetry))
+     (propagated-inputs (list python-textual-0.41))
+     (home-page
+      "https://github.com/fishinthecalculator/pot")
+     (synopsis
+      "")
+     (description "")
+     (license license:gpl3))))
+
+pot.git
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # `pot`
+
+## Contributing
+
+All contributions are welcome. If you have commit access please remember to setup the authentication hook with
+
+```bash
+cp etc/git/pre-push .git/hooks/pre-push
+```
+
+## License
+
+Unless otherwise stated all the files in this repository are to be considered under the GPL 3.0 terms. You are more than welcome to open issues or send patches.
