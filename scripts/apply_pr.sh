10ed759852825149eb4b08c9b75777111a92048e
Giacomo Leidi <giacomo.leidi@suse.com>
2023-11-09 00:30:14 +0100

    scripts: Add apply_pr.sh.

---
+++ scripts/apply_pr.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eu
+
+usage () {
+    echo "Usage: $(basename "$0") FORK-URL BRANCH" >&2
+}
+
+
+[ "$#" -ne 2 ] && usage && exit 1
+
+fork_url="$1"
+branch="$2"
+
+git checkout master
+
+# Setup
+git remote add forked "$fork_url"
+git fetch forked "${branch}"
+
+git cherry-pick -s "master..forked/${branch}"
+
+# Cleanup
+git branch -r -D "forked/${branch}"
+git remote remove forked
+
+# Run guix git authenticate
+guix shell make -- make authenticate
