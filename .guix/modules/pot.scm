0dd6696b2b38f056e1304e072eb8935ab8bb3fdd
Giacomo Leidi <giacomo.leidi@suse.com>
2024-03-29 01:50:12 +0100

    Release v0.0.5.

--- .guix/modules/pot.scm
+++ .guix/modules/pot.scm
@@ -26,7 +26,7 @@
 (define-public pot.git
   (package
    (name "pot.git")
-   (version "0.0.4")
+   (version "0.0.5")
    (source
     (local-file "../.." "pot-checkout"
                 #:recursive? #t
